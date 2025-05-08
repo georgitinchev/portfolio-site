0520a5f946ee15183455f03a18fab4d80204b9f1
Георги Тинчев <georgi.tinchev.124@gmail.com>
2025-05-08 23:11:33 +0300

    Portfolio v1.0: Complete implementation with all components and styling. Email JS integrated and all JSX components done. Only thing left is projects description and thumbnails

---
+++ portfolio-website/src/data/constants.js
@@ -0,0 +1,465 @@
+export const Bio = {
+  name: "Georgi Tinchev",
+  roles: [
+    "DevOps Engineer",
+    "Cloud Engineer",
+    "Full Stack Developer",
+    "IT Support Specialist",
+  ],
+  description:
+  "I'm an aspiring DevOps and Cloud Engineer from Sofia, Bulgaria. I’m passionate about optimizing infrastructure, automating workflows, and ensuring scalability using cloud platforms like AWS and Azure. I’m focused on building efficient, high-performance systems and enjoy exploring 3D design and music production in my free time.",
+  github: "https://github.com/georgitinchev",
+  resume: "https://drive.google.com/file/d/1opGQGy7nigej6707tYekTGXnhKLwKINv/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/georgi-tinchev-903446215/",
+  twitter: "https://twitter.com/georgi_tin",
+  insta: "https://www.instagram.com/georgi.tin/",
+  facebook: "https://www.facebook.com/georgi.ti/",
+  artstation: "https://www.artstation.com/echo_winter",
+  stackoverflow: "https://stackoverflow.com/users/10049923/george-tinchev"
+};
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name: "C#",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/csharp-colored.svg",
+      },
+      {
+        name: "JavaScript",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg",
+      },
+      {
+        name: "Python",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/python-colored.svg",
+      },
+      {
+        name: "TypeScript",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/typescript-colored.svg",
+      },
+    ],
+  },
+  {
+    title: "Web Development",
+    skills: [
+      {
+        name: "HTML5",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg",
+      },
+      {
+        name: "CSS3",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg",
+      },
+      {
+        name: "Sass",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/sass-colored.svg",
+      },
+      {
+        name: "TailwindCSS",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/tailwindcss-colored.svg",
+      },
+      {
+        name: "Bootstrap",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/bootstrap-colored.svg",
+      },
+      {
+        name: "three.js",
+        image: "https://canada1.discourse-cdn.com/flex035/uploads/threejs/optimized/2X/e/e4f86d2200d2d35c30f7b1494e96b9595ebc2751_2_1016x1024.png",
+      },
+    ],
+  },
+  {
+    title: "Frameworks & Libraries",
+    skills: [
+      {
+        name: "React",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg",
+      },
+      {
+        name: "Node.js",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg",
+      },
+      {
+        name: ".NET Core",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/1200px-.NET_Core_Logo.svg.png",
+      },
+      {
+        name: ".NET Framework",
+        image: "https://iconape.com/wp-content/files/pd/353150/svg/353150.svg",
+      },
+      {
+        name: "Jest",
+        image: "https://www.svgrepo.com/show/353930/jest.svg",
+      },
+      {
+        name: "Flask",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRubRv-E-PFnEuyYITdMYPqMXrBLOIKUTW8ug&s",
+      },
+    ],
+  },
+  {
+    title: "Databases",
+    skills: [
+      {
+        name: "MySQL",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mysql-colored.svg",
+      },
+      {
+        name: "SQL Server",
+        image: "https://img.icons8.com/?size=512&id=laYYF3dV0Iew&format=png",
+      },
+      {
+        name: "MongoDB",
+        image: "https://www.svgrepo.com/show/331488/mongodb.svg",
+      },
+    ],
+  },
+  {
+    title: "DevOps & Tools",
+    skills: [
+      {
+        name: "Microsoft Azure",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Microsoft_Azure.svg/2048px-Microsoft_Azure.svg.png",
+      },
+      {
+        name: "AWS",
+        image: "https://cdn.iconscout.com/icon/free/png-256/free-aws-logo-icon-download-in-svg-png-gif-file-formats--cloud-computing-network-server-database-brand-pack-logos-icons-1583149.png?f=webp&w=256",
+      },
+      {
+        name: "Git",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/git-colored.svg",
+      },
+      {
+        name: "GitLab",
+        image: "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/144_Gitlab_logo_logos-512.png",
+      },
+      {
+        name: "Docker",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/docker-colored.svg",
+      },
+      {
+        name: "Linux",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/linux-colored.svg",
+      },
+      {
+        name: "Bash",
+        image: "https://img.icons8.com/color/600/bash.png",
+      },
+      {
+        name: "Jenkins",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Jenkins_logo.svg/1200px-Jenkins_logo.svg.png",
+      },
+      {
+        name: "Kubernetes",
+        image: "https://static-00.iconduck.com/assets.00/kubernetes-icon-2048x1995-r1q3f8n7.png",
+      },
+      {
+        name: "Terraform",
+        image: "https://static-00.iconduck.com/assets.00/terraform-icon-1803x2048-hodrzd3t.png",
+      },
+      {
+        name: "Grafana",
+        image: "https://www.vectorlogo.zone/logos/grafana/grafana-icon.svg",
+      },
+      {
+        name: "Prometheus",
+        image: "https://www.vectorlogo.zone/logos/prometheusio/prometheusio-icon.svg",
+      },
+      {
+        name: "VMware Fusion",
+        image: "https://images.icon-icons.com/3053/PNG/512/vmware_fusion_macos_bigsur_icon_189580.png",
+      },
+    ],
+  },
+  {
+    title: "Design & Creative Tools",
+    skills: [
+      {
+        name: "Canva",
+        image: "https://play-lh.googleusercontent.com/3aWGqSf3T_p3F6wc8FFvcZcnjWlxpZdNaqFVEvPwQ1gTOPkVoZwq6cYvfK9eCkwCXbRY",
+      },
+      {
+        name: "Photoshop",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/photoshop-colored.svg",
+      },
+      {
+        name: "Premiere Pro",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/premierepro-colored.svg",
+      },
+      {
+        name: "Figma",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/figma-colored.svg",
+      },
+      {
+        name: "FL Studio",
+        image: "https://i.redd.it/szswuem4y3381.png",
+      },
+      {
+        name: "Ableton",
+        image: "https://images.icon-icons.com/3053/PNG/512/ableton_macos_bigsur_icon_190476.png",
+      },
+      {
+        name: "Final Cut Pro",
+        image: "https://img.icons8.com/?size=512&id=9vdP7gCiMiAA&format=png",
+      },
+      {
+        name: "Blender",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/blender-colored.svg",
+      },
+    ],
+  },
+  {
+    title: "Other Tools",
+    skills: [
+      {
+        name: "Arduino",
+        image: "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/arduino-colored.svg",
+      },
+      {
+        name: "PowerShell",
+        image: "https://upload.wikimedia.org/wikipedia/commons/2/2f/PowerShell_5.0_icon.png",
+      },
+      {
+        name: "Postman",
+        image: "https://cdn.creazilla.com/icons/3254217/postman-icon-lg.png",
+      },
+      {
+        name: "JetBrains Rider",
+        image: "https://resources.jetbrains.com/storage/products/rider/img/meta/rider_logo_300x300.png",
+      },
+      {
+        name: "Draw.io",
+        image: "https://static-00.iconduck.com/assets.00/drawio-icon-2048x2026-x5fvw1zs.png",
+      },
+      {
+        name: "VS Code",
+        image: "https://external-preview.redd.it/WSuAcyz1u8MoF8cokXspkmOIn8oWXaE8JH-SGXbUUW0.png?width=640&crop=smart&auto=webp&s=736c56fd482dc5cd058963102b35957e53be4b00",
+      },
+      {
+        name: "Visual Studio 2022",
+        image: "https://visualstudio.microsoft.com/wp-content/uploads/2021/10/Product-Icon.svg",
+      },
+      {
+        name: "PyCharm",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/PyCharm_Icon.svg/1024px-PyCharm_Icon.svg.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://image.pngaaa.com/878/1721878-middle.png",
+    role: "Junior IT Technician",
+    company: "SEGA",
+    date: "Nov 2024 – Mar 2025",
+    desc: "Managed Active Directory, ManageEngine, Office 365, and IT infrastructure. Provided IT support, troubleshooting, and employee onboarding. Worked with QA tools, ticketing systems, and system monitoring.",
+    skills: [
+      "Active Directory",
+      "Office 365",
+      "ManageEngine",
+      "Windows Support",
+      "IT Infrastructure",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM0qaDcUuVMXcqoo8dZdgGXNWPMWtn75kGEQ&s",
+    role: "Azure Identity Support Engineer",
+    company: "TeKnowledge (formerly Tek Experts)",
+    date: "June 2022 – May 2023",
+    desc: "Provided advanced support for Azure Identity Services including Active Directory, Azure AD, SSO, RBAC, and PIM. Automated tasks using PowerShell and contributed to technical documentation.",
+    skills: [
+      "Azure",
+      "Active Directory",
+      "PowerShell",
+      "SSO",
+      "RBAC",
+      "PIM",
+      "Troubleshooting",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://images.icon-icons.com/3781/PNG/512/upwork_icon_231982.png",
+    role: "Freelance Developer & IT Support",
+    company: "Freelance / Upwork",
+    date: "2022 – Present",
+    desc: "Delivered freelance development and cloud automation solutions for clients. Built tools and scripts, managed deployments, and consulted on IT issues. Completed various client projects via Upwork.",
+    skills: [
+      "Bash",
+      "Linux",
+      "Cloud Automation",
+      "Scripting",
+      "DevOps",
+      "Git",
+      "Remote Support",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://lens-storage.storage.googleapis.com/png/61e7ccf904bb4429b4cf2c57671c6f6d", // Fontys icon
+    school: "Fontys University of Applied Sciences, Eindhoven",
+    date: "2023 – 2024",
+    grade: "Propaedeutics Diploma",
+    desc: "Completed the first year (propaedeutics) in ICT & Software Engineering before deciding to pursue a more hands-on DevOps/cloud path.",
+    degree: "ICT & Software Engineering (Propaedeutics)",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdHVQmS_j5EnS20QAwcCz9J0rHoFz_oQgUdQ&s", // SoftUni icon
+    school: "Software University (SoftUni), Sofia",
+    date: "2022 – 2023",
+    grade: "Completed JavaScript Programming track",
+    desc: "Studied core JavaScript programming concepts including DOM manipulation, functions, objects, and algorithms.",
+    degree: "JavaScript Programming Course",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Flask API CRUD",
+    date: "2024",
+    description:
+      "A Python Flask REST API project integrated with SQLAlchemy, demonstrating how to build a fully functional REST API with CRUD operations and database management.",
+    tags: ["Python", "Flask", "SQLAlchemy", "REST API"],
+    category: "DevOps",
+    github: "https://github.com/georgitinchev/flask-api-crud"
+  },
+  {
+    id: 1,
+    title: "HashiCorp Vault Lab",
+    date: "2024",
+    description:
+      "A lab environment for experimenting with HashiCorp Vault to manage secrets and access control. Contains setup and configuration examples.",
+    tags: ["Vault", "DevOps", "Secrets Management"],
+    category: "DevOps",
+    github: "https://github.com/georgitinchev/hc-vault-lab"
+  },
+  {
+    id: 2,
+    title: "GitHub CI/CD Deployment Workflow",
+    date: "2024",
+    description:
+      "A simple GitHub Actions workflow for CI/CD: builds Java Gradle projects, pushes Docker images, and optionally deploys to GitHub Pages.",
+    tags: ["GitHub Actions", "CI/CD", "Docker", "Gradle"],
+    category: "DevOps",
+    github: "https://github.com/georgitinchev/gh-deployment-workflow"
+  },
+  {
+    id: 3,
+    title: "Docker Demo - Node.js User Profile App",
+    date: "2024",
+    description:
+      "A Node.js backend with Express, MongoDB, and a pure JavaScript frontend, all Dockerized for demonstration and testing.",
+    tags: ["Docker", "Node.js", "MongoDB", "JavaScript"],
+    category: "Web App",
+    github: "https://github.com/georgitinchev/docker-demo"
+  },
+  {
+    id: 4,
+    title: "Jenkins CI/CD Pipelines",
+    date: "2024",
+    description:
+      "Jenkins pipeline workflows for CI/CD automation. Includes both declarative and scripted pipelines.",
+    tags: ["Jenkins", "CI/CD", "DevOps"],
+    category: "DevOps",
+    github: "https://github.com/georgitinchev/jenkins-wf"
+  },
+  {
+    id: 5,
+    title: "Bash Script Exercises",
+    date: "2024",
+    description:
+      "A collection of Bash scripts and exercises to master Linux shell scripting. Includes examples from basic to advanced tasks.",
+    tags: ["Bash", "Linux", "Scripting"],
+    category: "DevOps",
+    github: "https://github.com/georgitinchev/bash-script-exercises"
+  },
+  {
+    id: 6,
+    title: "Log Archive Tool",
+    date: "2024",
+    description:
+      "A simple CLI tool to compress and archive logs from a specified directory. Useful for organizing and managing log files.",
+    tags: ["Bash", "Linux", "Logging"],
+    category: "DevOps",
+    github: "https://github.com/georgitinchev/log-archive-tool"
+  },
+  {
+    id: 7,
+    title: "Car Rental Web App",
+    date: "2024",
+    description:
+      "A PHP-based car rental management application with MySQL. Features include browsing, booking, and admin control over listings.",
+    tags: ["PHP", "MySQL", "Web App"],
+    category: "Web App",
+    github: "https://github.com/georgitinchev/car-rental-php"
+  },
+  {
+    id: 8,
+    title: "Media Bazaar Logistics System",
+    date: "2023",
+    description:
+      "Led a team to develop a logistics system with C# .NET. Features role-based management, stock tracking, task allocation, and analytics.",
+    tags: ["C#", ".NET", "Team Project", "Logistics"],
+    category: "Desktop App",
+    github: "https://github.com/georgitinchev/clubhouse-logistics-management",
+    member: [
+      {
+        name: "Boris Petrov",
+        github: "https://github.com/BorisPetrov777"
+      }
+    ]
+  },
+  {
+    id: 9,
+    title: "JavaScript Mini Projects",
+    date: "2023",
+    description:
+      "A collection of JavaScript projects including calculator, clock, compass, grocery list, phonebook, pong game, and weather app.",
+    tags: ["JavaScript", "API", "UI"],
+    category: "Web App",
+    github: "https://github.com/georgitinchev/js-projects"
+  },
+  {
+    id: 10,
+    title: "StreamSage – Movie Streaming App",
+    date: "2023",
+    description:
+      "Movie streaming platform built with C# .NET and .NET Core. Features movie recommendations, watchlists, and Razor Pages UI.",
+    tags: ["C#", ".NET Core", "SQL Server", "Razor Pages"],
+    category: "Desktop App",
+    github: "https://github.com/georgitinchev/streamsage-streamingservice"
+  },
+  {
+    id: 11,
+    title: "Library Management App",
+    date: "2023",
+    description:
+      "A C# WinForms application for managing a small library. Supports book search, member management, and JSON data export/import.",
+    tags: ["C#", "WinForms", "JSON"],
+    category: "Desktop App",
+    github: "https://github.com/georgitinchev/library-individual-project"
+  },
+  {
+    id: 12,
+    title: "Server Performance Stats Script",
+    date: "2024",
+    description:
+      "A Bash script to display key server performance metrics: CPU, memory, disk, and top processes. Ideal for quick health checks.",
+    tags: ["Bash", "Linux", "Monitoring"],
+    category: "DevOps",
+    github: "https://github.com/georgitinchev/server-performance-stats"
+  }
+];
+
+
+
+
