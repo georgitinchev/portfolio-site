ee16cbdc95330e585ad382eb60efbbcd608d170c
Георги Тинчев <georgi.tinchev.124@gmail.com>
2025-05-08 17:27:29 +0300

    Navbar basics

---
+++ portfolio-website/src/App.js
@@ -0,0 +1,15 @@
+import styled, { ThemeProvider } from "styled-components";
+import {darkTheme} from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Router } from "react-router-dom"
+
+
+function App() {
+  return <ThemeProvider theme={darkTheme}>
+    <BrowserRouter>
+    <Navbar />
+    </BrowserRouter>
+  </ThemeProvider>
+}
+
+export default App;
